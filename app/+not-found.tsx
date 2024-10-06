9e860f1de3bd06811902b819ab1f3dae821eeed2
michaelsun195 <44507393+michaelsun195@users.noreply.github.com>
2024-10-06 00:21:30 -0700

    Added citations for source code

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,3 +1,7 @@
+{/* This code follows the tutorial provided by Expo at
+    https://docs.expo.dev/tutorial/create-your-first-app/ and
+    https://docs.expo.dev/tutorial/add-navigation/ with some slight modifications. */}
+
 import { View, StyleSheet } from 'react-native';
 import { Link, Stack } from 'expo-router';
 
